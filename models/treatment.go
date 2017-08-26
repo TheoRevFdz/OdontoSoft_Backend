bd95f6d16de32462164c15357f1f57b3d5b7f3b9
TheoRev <hrevillafernandez79@gmail.com>
2017-08-26 10:13:19 -0500

    Correccion del json del modelo

--- models/treatment.go
+++ models/treatment.go
@@ -4,8 +4,8 @@ import "time"
 
 // Treatment estructura de la tabla tratamiento
 type Treatment struct {
-	ID         int64  `gorm:"type: serial; primary_key", json:"id"`
-	Work       string `gorm:"type: varchar(100), json:"work"`
+	ID         int64  `json:"id" gorm:"type: serial; primary_key"`
+	Work       string `json:"work" gorm:"type: varchar(100)"`
 	Quantity   int
 	Cost       float64 `gorm:"type:decimal(36,2)"`
 	CodPatient int64
