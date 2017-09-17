5491db2962aa172138ed0d6805756c9e07984744
TheoRev <hrevillafernandez79@gmail.com>
2017-09-17 18:19:09 -0500

    Agregando estilos

--- models/patient.go
+++ models/patient.go
@@ -1,25 +1,26 @@
 package models
 
 import "github.com/jinzhu/gorm"
+import "time"
 
 // Patient estructura de la tabla paciente
 type Patient struct {
 	gorm.Model
-	DateInit     string `json:"dateInit" gorm:"not null; type:date"`
-	NomApe       string `json:"nomApe" gorm:"not null; type:varchar(200)"`
-	Age          uint   `json:"age" gorm:"not null; type:integer"`
-	Sex          string `json:"sex" gorm:"not null; type:varchar(1)"`
-	DateNac      string `json:"dateNac" gorm:"not null; type:date"`
-	Address      string `json:"address" gorm:"type:varchar(200)"`
-	Ocupation    string `json:"ocupation" gorm:"not null; type:varchar(100)"`
-	TelCel       string `json:"telCel" gorm:"type:varchar(12)"`
-	Alergies     string `json:"alergies" gorm:"type:varchar(150)"`
-	Operations   string `json:"operations" gorm:"type:varchar(150)"`
-	Diabettes    bool   `json:"diabettes" gorm:"not null; type:boolean"`
-	Hipertension bool   `json:"hipertension" gorm:"not null; type:boolean"`
-	Others       string `json:"others" gorm:"type:varchar(120)"`
-	TreatMedics  string `json:"treatMedics" gorm:"type:varchar(120)"`
-	State        bool   `json:"state" gorm:"not null; type:boolean; default:TRUE"`
+	DateInit     time.Time `json:"dateInit" gorm:"not null"`
+	NomApe       string    `json:"nomApe" gorm:"not null; type:varchar(200)"`
+	Age          uint      `json:"age" gorm:"not null; type:integer"`
+	Sex          string    `json:"sex" gorm:"not null; type:varchar(1)"`
+	DateNac      time.Time `json:"dateNac" gorm:"not null"`
+	Address      string    `json:"address" gorm:"type:varchar(200)"`
+	Ocupation    string    `json:"ocupation" gorm:"not null; type:varchar(100)"`
+	TelCel       string    `json:"telCel" gorm:"type:varchar(12)"`
+	Alergies     string    `json:"alergies" gorm:"type:varchar(150)"`
+	Operations   string    `json:"operations" gorm:"type:varchar(150)"`
+	Diabettes    bool      `json:"diabettes" gorm:"not null; type:boolean"`
+	Hipertension bool      `json:"hipertension" gorm:"not null; type:boolean"`
+	Others       string    `json:"others" gorm:"type:varchar(120)"`
+	TreatMedics  string    `json:"treatMedics" gorm:"type:varchar(120)"`
+	State        bool      `json:"state" gorm:"not null; type:boolean; default:TRUE"`
 }
 
 // Patients slice de pacientes
--- public/js/controller/patient.js
+++ public/js/controller/patient.js
@@ -1,16 +1,35 @@
 (function () {
 	angular.module('patient.controller', [])
 		.controller('PatientController', ['$scope', '$http', function ($scope, $http) {
-			$scope.patients = {
+			$scope.patients = {};
 
-			};
+			findAllPatient($scope, $http);
 
-			$scope.loadPatients = function () {
-				findAllPatient($scope, $http);
-			}
+			$scope.loadPatients = function () {}
 		}]);
 
 	function findAllPatient($scope, $http) {
-		alert("Patients");
+		// console.log("Patients");
+
+		$http({
+			method: 'get',
+			url: 'api/patients/',
+			headers: 'Content-Type: application/json'
+		}).then(
+			function success(response) {
+				console.log(response.data);
+				$scope.patients = response.data;
+				// for(let i=0;i<response.data.length;i++){
+
+				// }
+			},
+			function error(response) {
+				alert(response);
+			}
+		).catch(
+			function fail(response) {
+				console.log("Excep: " + response);
+			}
+		);
 	}
 })();
